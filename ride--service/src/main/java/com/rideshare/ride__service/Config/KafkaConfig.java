4df388534cdb86f22d00c4a700b5cbec8aadbb09
pathakom09 <ompathak761@gmail.com>
2026-06-21 17:40:18 +0530

    kafka config, application.yml and exception handling

--- ride--service/src/main/java/com/rideshare/ride__service/Config/KafkaConfig.java
+++ ride--service/src/main/java/com/rideshare/ride__service/Config/KafkaConfig.java
@@ -1,4 +1,33 @@
 package com.rideshare.ride__service.Config;
 
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class KafkaConfig {
+
+    //Topic where Ride Service published ride request
+    // Matching Service subscribers to this topic
+
+    @Bean
+    public NewTopic rideRequestedTopic() {
+        return TopicBuilder.name("ride.requested")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+    //Topic where Matching Service publishes match results
+    // Ride Service subscribers to this topic
+
+    @Bean
+    public NewTopic rideMatchedTopic() {
+        return TopicBuilder.name("ride.matched")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
 }
